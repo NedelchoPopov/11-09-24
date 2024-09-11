048f6310f31fc6d2692f511d2598e48acc696c1d
Nedelcho <160885648+NedelchoPopov@users.noreply.github.com>
2024-09-11 15:25:46 +0300

    Add project files.

---
+++ CustomRandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new(); 
+            int index = random.Next(0, Count);
+
+            string elements = this[index];
+            RemoveAt(index);
+            return elements;
+        }
+    }
+}
---
+++ CustomRandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           RandomList strings = new RandomList 
+           { 
+            "nedelcho",
+            "popov",
+            "vasil",
+            "deni"
+           };
+
+            strings.RandomString();
+        }
+    }
+}
